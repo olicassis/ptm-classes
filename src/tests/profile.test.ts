ee60147793c9afe03f00d2bb6e619f71c36e6654
Elias Cyrino de Assis <eliascyrino@gmail.com>
2024-08-13 19:50:26 -0300

    test: add tests for get profile by id route

--- src/tests/profile.test.ts
+++ src/tests/profile.test.ts
@@ -45,3 +45,48 @@ describe('Get Profiles Test Suite', () => {
     expect(response.status).toEqual(500)
   })
 })
+
+describe('Get Profile By Id Test Suite', () => {
+  let typeorm: MockTypeORM
+
+  beforeAll(() => {
+    typeorm = new MockTypeORM()
+  })
+
+  afterEach(() => {
+    typeorm.resetAll()
+  })
+
+  afterAll(() => {
+    typeorm.restore()
+  })
+
+  test('Should return status 200 for a successfull call to get profile by id with existing profile', async () => {
+    typeorm.onMock(ProfileEntity).toReturn(mockedProfiles[0], 'findOneBy')
+    const response = await request(app).get(
+      '/api/profile/3a5ada5e-940f-439a-b333-bb4d48159cd9',
+    )
+    expect(response.body.data).toEqual(mockedProfiles[0])
+    expect(response.status).toEqual(200)
+  })
+
+  test('Should return status 200 for a successfull call to get profile by id with no existing profile', async () => {
+    typeorm.onMock(ProfileEntity).toReturn(null, 'findOneBy')
+    const response = await request(app).get(
+      '/api/profile/fa39f8bd-8672-4691-bb7e-978109af0f30',
+    )
+    expect(response.body.data).toBeNull()
+    expect(response.status).toEqual(200)
+  })
+
+  test('Should return status 500 when an error occurs calling get profile by id', async () => {
+    typeorm
+      .onMock(ProfileEntity)
+      .toReturn(new Error('An error has occurred'), 'findOneBy')
+    const response = await request(app).get(
+      '/api/profile/3a5ada5e-940f-439a-b333-bb4d48159cd9',
+    )
+    expect(response.body.data).toBeUndefined()
+    expect(response.status).toEqual(500)
+  })
+})
