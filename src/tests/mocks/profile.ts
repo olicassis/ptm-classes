d6e3b77348305ea94c0f54dabb88412492efebba
Elias Cyrino de Assis <eliascyrino@gmail.com>
2024-08-13 20:59:05 -0300

    test: add tests for get profile subjects route

--- src/tests/mocks/profile.ts
+++ src/tests/mocks/profile.ts
@@ -1,23 +1,31 @@
 import { ProfileEntity } from '../../db/entity/Profile'
+import { ProfileSubjectEntity } from '../../db/entity/ProfileSubject'
 import { ProfileRole, ProfileStatus } from '../../enums/profile'
 
-export const mockedProfiles: Partial<ProfileEntity>[] = [
-  {
-    id: '3a5ada5e-940f-439a-b333-bb4d48159cd9',
-    firstName: 'Dummy',
-    lastName: 'Student',
-    avatar: 'https://avatars.githubusercontent.com/u/bb4d48159cd9',
-    role: ProfileRole.STUDENT,
-    status: ProfileStatus.UNVERIFIED,
-    username: 'dummy_one',
-  },
-  {
-    id: 'ea123e3a-d922-4d37-833e-022375631298',
-    firstName: 'Dummy',
-    lastName: 'Teacher',
-    avatar: 'https://avatars.githubusercontent.com/u/022375631298',
-    role: ProfileRole.TEACHER,
-    status: ProfileStatus.VERIFIED,
-    username: 'dummy_two',
-  },
+const mockedStudentProfile = new ProfileEntity()
+mockedStudentProfile.id = '3a5ada5e-940f-439a-b333-bb4d48159cd9'
+mockedStudentProfile.firstName = 'Dummy'
+mockedStudentProfile.lastName = 'Student'
+mockedStudentProfile.avatar =
+  'https://avatars.githubusercontent.com/u/bb4d48159cd9'
+mockedStudentProfile.role = ProfileRole.STUDENT
+mockedStudentProfile.status = ProfileStatus.UNVERIFIED
+mockedStudentProfile.username = 'dummy_one'
+
+const mockedProfileSubject = new ProfileSubjectEntity()
+
+const mockedTeacherProfile = new ProfileEntity()
+mockedTeacherProfile.id = 'ea123e3a-d922-4d37-833e-022375631298'
+mockedTeacherProfile.firstName = 'Dummy'
+mockedTeacherProfile.lastName = 'Teacher'
+mockedTeacherProfile.avatar =
+  'https://avatars.githubusercontent.com/u/022375631298'
+mockedTeacherProfile.role = ProfileRole.TEACHER
+mockedTeacherProfile.status = ProfileStatus.VERIFIED
+mockedTeacherProfile.username = 'dummy_two'
+mockedTeacherProfile.profileSubject = [mockedProfileSubject]
+
+export const mockedProfiles: ProfileEntity[] = [
+  mockedStudentProfile,
+  mockedTeacherProfile,
 ]
--- src/tests/profile.test.ts
+++ src/tests/profile.test.ts
@@ -4,6 +4,7 @@ import request from 'supertest'
 
 import app from '../app'
 import { ProfileEntity } from '../db/entity/Profile'
+import { ProfileSubjectEntity } from '../db/entity/ProfileSubject'
 
 import { mockedProfiles } from './mocks/profile'
 
@@ -22,14 +23,14 @@ describe('Get Profiles Test Suite', () => {
     typeorm.restore()
   })
 
-  test('Should return status 200 for a successfull call to get profiles with existing profiles', async () => {
+  test('Should return status 200 for a successful call to get profiles with existing profiles', async () => {
     typeorm.onMock(ProfileEntity).toReturn(mockedProfiles, 'find')
     const response = await request(app).get('/api/profiles')
     expect(response.body.data).toEqual(mockedProfiles)
     expect(response.status).toEqual(200)
   })
 
-  test('Should return status 200 for a successfull call to get profiles with no existing profiles', async () => {
+  test('Should return status 200 for a successful call to get profiles with no existing profiles', async () => {
     typeorm.onMock(ProfileEntity).toReturn([], 'find')
     const response = await request(app).get('/api/profiles')
     expect(response.body.data.length).toEqual(0)
@@ -61,7 +62,7 @@ describe('Get Profile By Id Test Suite', () => {
     typeorm.restore()
   })
 
-  test('Should return status 200 for a successfull call to get profile by id with existing profile', async () => {
+  test('Should return status 200 for a successful call to get profile by id with existing profile', async () => {
     typeorm.onMock(ProfileEntity).toReturn(mockedProfiles[0], 'findOneBy')
     const response = await request(app).get(
       '/api/profile/3a5ada5e-940f-439a-b333-bb4d48159cd9',
@@ -70,7 +71,7 @@ describe('Get Profile By Id Test Suite', () => {
     expect(response.status).toEqual(200)
   })
 
-  test('Should return status 200 for a successfull call to get profile by id with no existing profile', async () => {
+  test('Should return status 200 for a successful call to get profile by id with no existing profile', async () => {
     typeorm.onMock(ProfileEntity).toReturn(null, 'findOneBy')
     const response = await request(app).get(
       '/api/profile/fa39f8bd-8672-4691-bb7e-978109af0f30',
@@ -85,6 +86,53 @@ describe('Get Profile By Id Test Suite', () => {
       .toReturn(new Error('An error has occurred'), 'findOneBy')
     const response = await request(app).get(
       '/api/profile/3a5ada5e-940f-439a-b333-bb4d48159cd9',
+    )
+    expect(response.body.data).toBeUndefined()
+    expect(response.status).toEqual(500)
+  })
+})
+
+describe('Get Profile Subjects By Profile Id Test Suite', () => {
+  let typeorm: MockTypeORM
+
+  beforeAll(() => {
+    typeorm = new MockTypeORM()
+  })
+
+  afterEach(() => {
+    typeorm.resetAll()
+  })
+
+  afterAll(() => {
+    typeorm.restore()
+  })
+
+  test('Should return status 200 for a successful call to get profile with an associated profile subject', async () => {
+    typeorm
+      .onMock(ProfileSubjectEntity)
+      .toReturn(mockedProfiles[1].profileSubject, 'findBy')
+    const response = await request(app).get(
+      '/api/profile/ea123e3a-d922-4d37-833e-022375631298/subjects',
+    )
+    expect(response.body.data).toEqual(mockedProfiles[1].profileSubject)
+    expect(response.status).toEqual(200)
+  })
+
+  test('Should return status 200 for a successful call to get profile with no associated profile subject', async () => {
+    typeorm.onMock(ProfileSubjectEntity).toReturn([], 'findBy')
+    const response = await request(app).get(
+      '/api/profile/3a5ada5e-940f-439a-b333-bb4d48159cd9/subjects',
+    )
+    expect(response.body.data.length).toEqual(0)
+    expect(response.status).toEqual(200)
+  })
+
+  test('Should return status 500 when an error occurs calling get profile subjects by profile id', async () => {
+    typeorm
+      .onMock(ProfileSubjectEntity)
+      .toReturn(new Error('An error has occurred'), 'findBy')
+    const response = await request(app).get(
+      '/api/profile/ea123e3a-d922-4d37-833e-022375631298/subjects',
     )
     expect(response.body.data).toBeUndefined()
     expect(response.status).toEqual(500)
