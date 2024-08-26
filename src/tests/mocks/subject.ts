7c9f906115bc06ba0c9f1f2032344302fbc0c8d3
Elias Cyrino de Assis <eliascyrino@gmail.com>
2024-08-26 20:14:52 -0300

    test: add tests for create profile subject route

---
+++ src/tests/mocks/subject.ts
@@ -0,0 +1,18 @@
+import { ProfileSubjectEntity } from '../../db/entity/ProfileSubject'
+import { CreateSubjectRequest } from '../../middlewares/validators/subject'
+
+import { mockedProfiles } from './profile'
+
+export const mockedCreateProfileSubjectInputValid: CreateSubjectRequest = {
+  profileId: mockedProfiles[1].id,
+  label: 'Biology',
+}
+
+export const mockedProfileSubject = new ProfileSubjectEntity()
+mockedProfileSubject.id = '8a83d823-ec03-46f2-bee8-63afa7938f8c'
+mockedProfileSubject.label = 'biology'
+mockedProfileSubject.profileId = mockedProfiles[1].id
+
+export const mockedCreateProfileSubjectInputInvalid = {
+  label: 'Invalid Test',
+} as CreateSubjectRequest
---
+++ src/tests/subject.test.ts
@@ -0,0 +1,108 @@
+import { describe, test } from '@jest/globals'
+import { MockTypeORM } from 'mock-typeorm'
+import request from 'supertest'
+
+import app from '../app'
+import { ProfileEntity } from '../db/entity/Profile'
+import { ProfileSubjectEntity } from '../db/entity/ProfileSubject'
+
+import { mockedProfiles } from './mocks/profile'
+import {
+  mockedCreateProfileSubjectInputInvalid,
+  mockedCreateProfileSubjectInputValid,
+  mockedProfileSubject,
+} from './mocks/subject'
+
+describe('Post Subject Test Suite', () => {
+  let typeorm: MockTypeORM
+
+  beforeAll(() => {
+    typeorm = new MockTypeORM()
+  })
+
+  afterEach(() => {
+    typeorm.resetAll()
+  })
+
+  afterAll(() => {
+    typeorm.restore()
+  })
+
+  test('Should return status 200 for a successful call to post subject', async () => {
+    typeorm.onMock(ProfileEntity).toReturn(mockedProfiles[1], 'findOneBy')
+    typeorm
+      .onMock(ProfileSubjectEntity)
+      .toReturn([mockedProfileSubject], 'save')
+    const response = await request(app)
+      .post('/api/subject')
+      .send(mockedCreateProfileSubjectInputValid)
+    expect(response.body.data).toEqual(mockedProfileSubject)
+    expect(response.status).toEqual(200)
+  })
+
+  test('Should return status 404 for a non existing profile', async () => {
+    typeorm.onMock(ProfileEntity).toReturn(null, 'findOneBy')
+    const response = await request(app)
+      .post('/api/subject')
+      .send(mockedCreateProfileSubjectInputValid)
+    expect(response.body.data).toBeUndefined()
+    expect(response.status).toEqual(404)
+  })
+
+  test('Should return status 403 if profile is not a teacher profile', async () => {
+    typeorm.onMock(ProfileEntity).toReturn(mockedProfiles[0], 'findOneBy')
+    const response = await request(app)
+      .post('/api/subject')
+      .send(mockedCreateProfileSubjectInputValid)
+    expect(response.body.data).toBeUndefined()
+    expect(response.status).toEqual(403)
+  })
+
+  test('Should return status 500 when an error occurs creating a subject', async () => {
+    typeorm.onMock(ProfileEntity).toReturn(mockedProfiles[1], 'findOneBy')
+    typeorm
+      .onMock(ProfileSubjectEntity)
+      .toReturn(new Error('An error has occurred creating subject'), 'save')
+    const response = await request(app)
+      .post('/api/subject')
+      .send(mockedCreateProfileSubjectInputValid)
+    expect(response.body.data).toBeUndefined()
+    expect(response.status).toEqual(500)
+  })
+
+  test('Should return status 500 when no subject is created - null', async () => {
+    typeorm.onMock(ProfileEntity).toReturn(mockedProfiles[1], 'findOneBy')
+    typeorm.onMock(ProfileSubjectEntity).toReturn(null, 'save')
+    const response = await request(app)
+      .post('/api/subject')
+      .send(mockedCreateProfileSubjectInputValid)
+    expect(response.body.data).toBeUndefined()
+    expect(response.status).toEqual(500)
+  })
+
+  test('Should return status 500 when no subject is created - null', async () => {
+    typeorm.onMock(ProfileEntity).toReturn(mockedProfiles[1], 'findOneBy')
+    typeorm.onMock(ProfileSubjectEntity).toReturn([], 'save')
+    const response = await request(app)
+      .post('/api/subject')
+      .send(mockedCreateProfileSubjectInputValid)
+    expect(response.body.data).toBeUndefined()
+    expect(response.status).toEqual(500)
+  })
+
+  test('Should return a validation error if payload is invalid', async () => {
+    const response = await request(app)
+      .post('/api/subject')
+      .send(mockedCreateProfileSubjectInputInvalid)
+    expect(response.status).toEqual(400)
+    expect(response.body).toMatchObject([
+      {
+        instancePath: '',
+        keyword: 'required',
+        message: "must have required property 'profileId'",
+        params: { missingProperty: 'profileId' },
+        schemaPath: '#/required',
+      },
+    ])
+  })
+})
